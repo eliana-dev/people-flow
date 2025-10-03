78a41e5ca74936ec66ca37c63299ec346f85a1df
Eliana <valdezeliana38@gmail.com>
2025-10-03 11:52:35 -0300

    Add environment variable support for MongoDB configuration

--- src/config.py
+++ src/config.py
@@ -1,5 +1,10 @@
-MONGO_HOST = "localhost"
-MONGO_PORT = 27017
-MONGO_USER = "pepito"
-MONGO_PASS = "perrito"
-MONGO_AUTH_MECHANISM = "SCRAM-SHA-256"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_HOST = (os.getenv("MONGO_HOST"),)
+MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASS = os.getenv("MONGO_PASS")
+MONGO_AUTH_MECHANISM = os.getenv("MONGO_AUTH_MECHANISM", "SCRAM-SHA-256")
