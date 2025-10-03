36fee34a2c8abe45a88e6de996df37a15634b0f8
Eliana <valdezeliana38@gmail.com>
2025-10-03 06:10:40 -0300

    Add Empleado and EmpleadoUpdate models for employee management

---
+++ src/models/empleados.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+
+class Empleado(BaseModel):
+    nombre: str  # Si no se le pone un valor por defecto entonces considera que es obligatirio
+    apellido: str
+    email: str
+    puesto: str
+    salario: float
+    fecha_ingreso: datetime = Field(default_factory=datetime.now)
+
+
+class EmpleadoUpdate(BaseModel): # modelo usado para actualizar, si no se pasa los valores toma none por defecto
+    nombre: str = None
+    apellido: str = None
+    email: str = None
+    puesto: str = None
+    salario: float = None
